e9dfbb1b4dc38c43ac1aa0118a386a94895050a9
osy1223 <osu1223@gmail.com>
2020-12-09 10:01:50 +0900

    12-09

--- TeamProject/data.py
+++ TeamProject/data.py
@@ -1,134 +1,90 @@
-# import numpy as np
-# import os
-# from tensorflow.keras.preprocessing import image
-# from tensorflow.keras.preprocessing.image import ImageDataGenerator
-# from tensorflow.keras.models import Sequential, Model
-# from tensorflow.keras.layers import Conv2D, Dense, Flatten, MaxPooling2D, LeakyReLU, Dropout, Input, Concatenate, BatchNormalization, Activation
-# from tensorflow.keras.layers import Conv2DTranspose
-# from tensorflow.keras.optimizers import Adam
-# from tensorflow.keras.initializers import RandomNormal
-# from sklearn.model_selection import train_test_split
-# import matplotlib.pyplot as plt
-# from numpy import savez_compressed
-
-# sketch_path="./data/sketch/"
-# photo_path="./data/photo/"
-
-# #이미지 이름 저장
-# camel_sketch_idx = next(os.walk("./data/sketch/camel/"))[2]
-# camel_photo_idx = next(os.walk("./data/photo/camel/"))[2]
-
-# camel_sketch = np.zeros((len(camel_sketch_idx),256,256,3),dtype=np.uint8) 
-# camel_photo = np.zeros((len(camel_photo_idx),256,256,3),dtype=np.uint8)
-
-# from tqdm import tqdm_notebook
-# from keras.preprocessing.image import img_to_array, load_img
-# from skimage.transform import resize #사이즈 조절
-
-# def sketch_image(sketch_idx, name, sketch) :
-#     for i, idx in tqdm_notebook(enumerate(sketch_idx),total=len(sketch_idx)):
-#         print("i : ",i)
-#         print("idx : ",idx)
-#         img = load_img("./data/sketch/"+name+"/"+idx)
-#         img = img_to_array(img)
-#         sketch[i] = img
-#     return sketch
-
-# def photo_image(sketch_idx, name, photo) :
-#     for i, idx in tqdm_notebook(enumerate(sketch_idx),total=len(sketch_idx)):
-#         print("i : ",i)
-#         print("idx : ",idx)
-#         idx = idx.split("-")[0]
-#         img = load_img("./data/photo/"+name+"/"+ idx)
-#         img = img_to_array(img)
-#         photo[i] = img
-#     return photo
-
-
-# #sketch: X, photo: Y
-# np.save('./npySAVE/sketch_camel.npy', arr=sketch_image(camel_sketch_idx, 'camel', camel_sketch))
-# np.save('./npySAVE/photo_camel.npy', arr=photo_image(camel_photo_idx, 'camel', camel_photo))
-
-
-# filename = 'sketch_to_photo.npz'
-# np.savez_compressed(filename, sketch_image(camel_sketch_idx, 'camel', camel_sketch), photo_image(camel_photo_idx, 'camel', camel_photo))
-# print('Saved dataset:', filename) #Saved dataset: sketch_to_photo.npz
-
 import numpy as np
 import os
-import glob
-
-a = glob.glob("./data/sketch/*/*")
 
 
-sketch_idx = []
-for i in range(len(a)):
+#이미지 이름 저장
+camel_photo_idx0 = next(os.walk("./TeamProject/data/photo/camel/"))[2]
+camel_photo_idx1 = next(os.walk("./TeamProject/data/photo/camel/"))[2]
+camel_sketch_idx0 = next(os.walk("./TeamProject/data/sketch/0/camel/"))[2]
+camel_sketch_idx1 = next(os.walk("./TeamProject/data/sketch/1/camel/"))[2]
 
-    c = a[i].split("\\")[1:]
-    c = c[0]+"/"+c[1]
+door_photo_idx0 = next(os.walk("./TeamProject/data/photo/door/"))[2]
+door_photo_idx1 = next(os.walk("./TeamProject/data/photo/door/"))[2]
+door_sketch_idx0 = next(os.walk("./TeamProject/data/sketch/0/door/"))[2]
+door_sketch_idx1 = next(os.walk("./TeamProject/data/sketch/1/door/"))[2]
 
-    sketch_idx.append(c)
+camel_sketch0 = np.zeros((len(camel_sketch_idx0),256,256,3),dtype=np.uint8) 
+camel_sketch1 = np.zeros((len(camel_sketch_idx1),256,256,3),dtype=np.uint8) 
+camel_photo0 = np.zeros((len(camel_sketch_idx0),256,256,3),dtype=np.uint8)
+camel_photo1 = np.zeros((len(camel_sketch_idx1),256,256,3),dtype=np.uint8)
 
-print(len(sketch_idx)) #6425
+door_sketch0 = np.zeros((len(door_sketch_idx0),256,256,3),dtype=np.uint8) 
+door_sketch1 = np.zeros((len(door_sketch_idx1),256,256,3),dtype=np.uint8) 
+door_photo0 = np.zeros((len(door_sketch_idx0),256,256,3),dtype=np.uint8)
+door_photo1 = np.zeros((len(door_sketch_idx1),256,256,3),dtype=np.uint8)
 
-
-a = glob.glob("./data/photo/*/*")
-
-
-photo_idx = []
-for i in range(len(a)):
-
-    c = a[i].split("\\")[1:]
-    c = c[0]+"/"+c[1]
-
-    photo_idx.append(c)
-
-print(len(photo_idx)) #1000
-
-
-#메모리를 적게 쓰기 위해 uint8로 
-sketch = np.zeros((len(sketch_idx),256,256,3),dtype=np.uint8) 
-photo = np.zeros((len(sketch_idx),256,256,3),dtype=np.uint8)
 
 from tqdm import tqdm_notebook
 from keras.preprocessing.image import img_to_array, load_img
 from skimage.transform import resize #사이즈 조절
 
-for i, idx in (enumerate(sketch_idx)):
-    print("i : ",i)
-    print("idx : ",idx)
-    img = load_img("./data/sketch/"+idx)
-    img = img_to_array(img)
-    sketch[i] = img
+def sketch_image(sketch_idx, name, sketch) :
+    for i, idx in tqdm_notebook(enumerate(sketch_idx),total=len(sketch_idx)):
+        print("i : ",i)
+        print("idx : ",idx)
+        img = load_img("./TeamProject/data/sketch/"+name+"/"+idx)
+        img = img_to_array(img)
+        sketch[i] = img
+    return sketch
 
-#sketch에 맞춰서 photo를 늘림 
-for i, idx in (enumerate(sketch_idx)):
-    print("i : ",i)
-    print("idx : ",idx)
-    idx = idx.split("-")[0]
-    img = load_img("./data/photo/"+ idx+".jpg")
-    img = img_to_array(img)
-    photo[i] = img
-
-
-
-
-print(sketch.shape) #(690, 256, 256, 3)
-print(photo.shape) #(690, 256, 256, 3)
-
-print(photo[0])
-print("===============================")
-print(photo[1])
-
-print(photo[0] == photo[1])
+def photo_image(sketch_idx, name, photo) :
+    for i, idx in tqdm_notebook(enumerate(sketch_idx),total=len(sketch_idx)):
+        print("i : ",i)
+        print("idx : ",idx)
+        idx = idx.split("-")[0]
+        img = load_img("./TeamProject/data/photo/"+name+"/"+ idx+".jpg")
+        img = img_to_array(img)
+        photo[i] = img
+    return photo
 
 
-#*npz = 압축된 npy?
-from numpy import savez_compressed
+
+camel_sketch1=sketch_image(camel_sketch1_idx, '0', 'camel', camel_sketch1)
+camel_photo1=photo_image(camel_sketch1_idx, 'camel', camel_photo1)
+door_sketch1=sketch_image(door_sketch1_idx, '0', 'door', door_sketch1)
+door_photo1=photo_image(door_sketch1_idx, 'door', door_photo1)
+
+camel_sketch2=sketch_image(camel_sketch2_idx, '1', 'camel', camel_sketch2)
+camel_photo2=photo_image(camel_sketch2_idx, 'camel', camel_photo2)
+door_sketch2=sketch_image(door_sketch2_idx, '1', 'door', door_sketch2)
+door_photo2=photo_image(door_sketch2_idx, 'door', door_photo2)
+
+camel_sketch3=sketch_image(camel_sketch3_idx, '2', 'camel', camel_sketch3)
+camel_photo3=photo_image(camel_sketch3_idx, 'camel', camel_photo3)
+door_sketch3=sketch_image(door_sketch3_idx, '2', 'door', door_sketch3)
+door_photo3=photo_image(door_sketch3_idx, 'door', door_photo3)
 
 #sketch: X, photo: Y
-# np.save('./data/sketch_camel.npy', arr=sketch)
-# np.save('./data/photo_camel.npy', arr=photo)
+np.save('./npySAVE/sketch_camel1.npy', arr=camel_sketch1)
+np.save('./npySAVE/photo_camel1.npy', arr=camel_photo1)
+np.save('./npySAVE/sketch_door1.npy', arr=door_sketch1)
+np.save('./npySAVE/photo_door1.npy', arr=door_photo1)
 
-filename = 'data_256.npz'
-np.savez_compressed(filename, sketch, photo)
+np.save('./npySAVE/sketch_camel2.npy', arr=camel_sketch2)
+np.save('./npySAVE/photo_camel2.npy', arr=camel_photo2)
+np.save('./npySAVE/sketch_door2.npy', arr=door_sketch2)
+np.save('./npySAVE/photo_door2.npy', arr=door_photo2)
+
+np.save('./npySAVE/sketch_camel3.npy', arr=camel_sketch3)
+np.save('./npySAVE/photo_camel3.npy', arr=camel_photo3)
+np.save('./npySAVE/sketch_door3.npy', arr=door_sketch3)
+np.save('./npySAVE/photo_door3.npy', arr=door_photo3)
+
+
+filename = 'sketch_to_photo.npz'
+np.savez_compressed(filename, camel_sketch1, camel_photo1,
+                            door_sketch1, door_photo1,
+                            camel_sketch2, camel_photo2,
+                            door_sketch2, door_photo2,
+                            camel_sketch3, camel_photo3,
+                            door_sketch3, door_photo3)
--- keras/keras19_GRU2.py
+++ keras/keras19_GRU2.py
@@ -17,10 +17,10 @@ x = x.reshape(13,3,1)
 
 # 2. 모델 구성
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, LSTM, SimpleRNN
+from tensorflow.keras.layers import Dense, GRU
 
 model = Sequential()
-model.add(SimpleRNN(10, activation='relu', input_shape=(3,1)))
+model.add(GRU(10, activation='relu', input_shape=(3,1)))
 model.add(Dense(20))
 model.add(Dense(10))
 model.add(Dense(1))
