e3948e86542bbda0342d4dc79d9222e841847b57
osy1223 <osu1223@gmail.com>
2020-12-16 12:01:39 +0900

    12-16

---
+++ ae/a01_autoencoder.py
@@ -0,0 +1,66 @@
+import numpy as np
+from tensorflow.keras.datasets import mnist
+
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+(x_train, _), (x_test, _) = mnist.load_data()
+
+x_train = x_train.reshape(60000, 784).astype('float32')/255
+x_test = x_test.reshape(10000, 784)/255.
+
+print(x_train[0])
+print(x_test[0])
+
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Input
+
+input_img = Input(shape=(784,))
+encoded = Dense(64, activation='relu')(input_img)
+decoded = Dense(784, activation='sigmoid')(encoded)
+
+autoencoder = Model(input_img, decoded)
+
+autoencoder.summary()
+'''
+Model: "functional_1"
+_________________________________________________________________
+Layer (type)                 Output Shape              Param #
+=================================================================
+input_1 (InputLayer)         [(None, 784)]             0
+_________________________________________________________________
+dense (Dense)                (None, 64)                50240
+_________________________________________________________________
+dense_1 (Dense)              (None, 784)               50960
+=================================================================
+Total params: 101,200
+Trainable params: 101,200
+Non-trainable params: 0
+_________________________________________________________________
+'''
+
+# autoencoder.compile(optimizer='adam', loss='mse')
+autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
+
+
+autoencoder.fit(x_train, x_train, epochs=50, batch_size=256, validation_split=0.2)
+# y값이 없으므로, 본인 자신으로 훈련
+
+decoded_img = autoencoder.predict(x_test)
+#원래 것과 비교
+
+import matplotlib.pyplot as plt
+n = 10
+plt.figure(figsize=(20,4))
+for i in range(n):
+    ax = plt.subplot(2, n, i+1)
+    plt.imshow(x_test[i].reshape(28,28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+    ax = plt.subplot(2, n, i+1+n)
+    plt.imshow(decoded_img[i].reshape(28,28))
+    plt.gray()
+    ax.get_xaxis().set_visible(False)
+    ax.get_yaxis().set_visible(False)
+
+plt.show()
